a3fde08a23985efb44fedf4f73c11b8e5728bab1
suryakante <suryakante888@gmail.com>
2018-04-03 18:01:39 +0530

    Drools with Decision Table

---
+++ Drools-KIE-XLS/src/main/java/com/centurylink/main/DroolsTest.java
@@ -0,0 +1,35 @@
+package com.centurylink.main;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.rule.FactHandle;
+
+import com.centurylink.model.Product;
+
+public class DroolsTest {
+
+	public static final void main(String[] args) {
+		try {
+			KieServices ks = KieServices.Factory.get();
+			KieContainer kContainer = ks.getKieClasspathContainer();
+
+			KieSession kSession = kContainer.newKieSession("ksession-rule");
+
+			Product product = new Product();
+			product.setType("diamond");
+
+			FactHandle fact1;
+
+			fact1 = kSession.insert(product);
+			kSession.fireAllRules();
+
+			System.out.println("The discount for the jewellery product "
+					+ product.getType() + " is " + product.getDiscount());
+
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+
+}
